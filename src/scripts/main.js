cb4679b23a8f855e759cc45ffb81037bcca6a10b
Hanna <hannachachacha7@gmail.com>
2025-04-02 17:06:56 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const employeeList = document.querySelector('ul');
+
+function sortList(list) {
+  const listItems = [...list.querySelectorAll('li')];
+
+  function getSalary(li) {
+    return Number(li.dataset.salary.replace(/[$,]/g, ''));
+  }
+
+  listItems.sort((a, b) => getSalary(b) - getSalary(a));
+
+  list.innerHTML = '';
+  listItems.forEach((li) => list.appendChild(li));
+}
+
+sortList(employeeList);
